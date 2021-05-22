f5890f04516a99be764005bec873455618669036
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-05-22 22:26:45 +0100

    Write interface code

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+// interfaces
+interface IsPerson {
+  name: string,
+  age: number,
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'Shaun',
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent', amount);
+    return amount;
+  }
+};
+
+console.log(me);
+
+const greetPerson = (person: IsPerson) => {
+  console.log('Hello', person.name);
+}
+
+greetPerson(me);
+
 import { Invoice } from './classes/invoice.js';
 
 const invOne = new Invoice('Mario', 'Work on the Mario website', 250);
